2a23aafab2035249de8711d82941676c2fa01b7b
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2019-12-10 11:45:15 -0500

    upped ppn to 8

--- code/scripts/searchlights/searchlight_config.py
+++ code/scripts/searchlights/searchlight_config.py
@@ -17,7 +17,7 @@ config['lockdir'] = os.path.join(config['workingdir'], 'locks')
 config['jobname'] = "searchlight"  # default job name
 config['q'] = "largeq"  # options: default, test, largeq
 config['nnodes'] = 1  # how many nodes to use for this one job
-config['ppn'] = 4  # how many processors to use for this one job (assume 4GB of RAM per processor)
+config['ppn'] = 8  # how many processors to use for this one job (assume 4GB of RAM per processor)
 config['walltime'] = '10:00:00'  # maximum runtime, in h:MM:SS
 config['cmd_wrapper'] = "python"  # replace with actual command wrapper (e.g. matlab, python, etc.)
 
