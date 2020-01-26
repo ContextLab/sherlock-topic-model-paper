415812130a98af0e06447b34875d965c84ecef30
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-01-26 13:37:42 -0500

    updated script for loading cluster output

--- code/scripts/load_cluster_output.py
+++ code/scripts/load_cluster_output.py
@@ -6,10 +6,11 @@ from os.path import join as opj
 hostname, username, password = sys.argv[1:]
 
 datadir_local = '../../data/processed/'
-emb_path_remote = '/dartfs/rc/lab/D/DBIC/CDL/f0028ph/sherlock-embedding/data/optimized/embeddings.npy'
+emb_path_remote = f'/dartfs/rc/lab/D/DBIC/CDL/{username}/sherlock-embedding/data/optimized/embeddings.npy'
 emb_path_local = opj(datadir_local, 'embeddings.npy')
 
-sl_dir_remote = '/dartfs/rc/lab/D/DBIC/CDL/f0028ph/searchlight-sherlock/data/finished/'
+sl_dir_remote = f'/dartfs/rc/lab/D/DBIC/CDL/{username}/searchlights/data/'
+sl_output_dr = opj(sl_dir_remote, 'finished')
 sl_vdir_local = opj(datadir_local, 'searchlight_video')
 sl_rdir_local = opj(datadir_local, 'searchlight_recall')
 
@@ -23,9 +24,10 @@ with spurplus.connect_with_retries(hostname=hostname,
                                    password=password) as remote:
                                # copy over embeddings
                                remote.get(emb_path_remote, emb_path_local)
+
                                # copy over searchlights
                                for analysis in ['video', 'recall']:
-                                   src_dir = opj(sl_dir_remote, f'searchlight_{analysis}')
+                                   src_dir = opj(sl_output_dr, f'searchlight_{analysis}')
                                    dest_dir = opj(datadir_local, f'searchlight_{analysis}')
                                    perm_dir = opj(src_dir, 'perms')
                                    for sub in range(1, 18):
@@ -38,3 +40,7 @@ with spurplus.connect_with_retries(hostname=hostname,
                                        perm_src_path = opj(perm_dir, f'perm{perm}.nii.gz')
                                        perm_dest_path = opj(dest_dir, f'perm{perm}.nii.gz')
                                        remote.get(perm_src_path, perm_dest_path)
+
+                                # copy over reference image
+                                src_path = opj(sl_dir_remote, 'inputs', 'fMRI', 'ref_img.nii.gz')
+                                dest_path = opj(sl_vdir_local, 'ref_img.nii.gz')
